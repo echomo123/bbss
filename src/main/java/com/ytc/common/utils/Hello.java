e83acf0988af71ed1343a8a1624aa09500860126
echomo123 <1462401835@qq.com>
2019-04-13 01:45:49 +0800

    first commit

---
+++ src/main/java/com/ytc/common/utils/Hello.java
@@ -0,0 +1,13 @@
+package com.ytc.common.utils;
+
+/**
+ * Created by Administrator on 2019/4/13.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        String a = "hello,world!";
+        System.out.print("------------------"+a);
+    }
+
+}
